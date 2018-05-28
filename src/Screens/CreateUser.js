60754dcd6b898a4983ddc9ca1a2e0a066f0c7c6d
AJ Genung <DragonRydrz@me.com>
2018-05-27 23:38:19 -0400

    Started security updates.
    Added choice of 3 security questions in the CreateUser.js screen.

--- src/Screens/CreateUser.js
+++ src/Screens/CreateUser.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { SafeAreaView, View, Text } from 'react-native';
+import ModalSelector from 'react-native-modal-selector';
 
+import { securityQuestions } from '../host';
 import { loading } from '../actions/loading';
 import { createUser } from '../actions/createUser';
 import {
@@ -20,6 +22,8 @@ class CreateUser extends Component {
     password: '',
     confirmPassword: '',
     error: '',
+    securityQuestion: '',
+    securityResponse: '',
   };
   // }
   render() {
@@ -55,6 +59,28 @@ class CreateUser extends Component {
               }
             />
           </CardSection>
+          <CardSection>
+            <View style={{ width: '100%', justifyContent: 'center' }}>
+              <ModalSelector
+                data={securityQuestions}
+                initValue="Tap to Choose a Security Question"
+                onChange={option => {
+                  console.log(option.label);
+                  this.setState({ securityQuestion: option.label });
+                }}
+              />
+            </View>
+          </CardSection>
+          <CardSection>
+            <Input
+              placeholder="security response"
+              label="Answer"
+              value={this.state.securityResponse}
+              onChangeText={securityResponse =>
+                this.setState({ securityResponse })
+              }
+            />
+          </CardSection>
 
           {this.state.error ? (
             <Text style={styles.errorTextStyle}>{this.state.error}</Text>
@@ -79,17 +105,40 @@ class CreateUser extends Component {
     this.setState({ error: '' });
     const { password, confirmPassword } = this.state;
 
-    this.state.password === this.state.confirmPassword
-      ? this.passwordsMatch()
-      : this.setState({ error: 'Passwords do not match!' });
+    if (this.state.username.length < 4) {
+      this.setState({ error: 'Username must be at least 4 characters.' });
+      this.props.loading(false);
+    } else if (this.state.password.length < 8) {
+      this.setState({ error: 'Password must be at least 8 characters.' });
+      this.props.loading(false);
+    } else if (this.state.password !== this.state.confirmPassword) {
+      this.setState({ error: 'Passwords do not match!' });
+      this.props.loading(false);
+    } else if (this.state.securityQuestion === '') {
+      this.setState({ error: 'Please choose a security question.' });
+      this.props.loading(false);
+    } else if (this.state.securityResponse.length < 4) {
+      this.setState({ error: 'Answer must be at least 4 characters.' });
+      this.props.loading(false);
+    } else if (
+      this.state.password === this.state.confirmPassword &&
+      this.state.password !== ''
+    ) {
+      this.passwordsMatch();
+    }
   };
   passwordsMatch = () => {
     const user = {
       username: this.state.username,
       password: this.state.password,
+      security: {
+        securityQuestion: this.state.securityQuestion,
+        securityResponse: this.state.securityResponse.toLowerCase(),
+      },
     };
     this.setState({ username: '', password: '', confirmPassword: '' });
     this.props.createUser(user, this.props.navigation.navigate);
+    this.props.loading(false);
   };
 }
 
--- src/host.js
+++ src/host.js
@@ -1,2 +1,18 @@
 const host = 'https://ajlnbe.herokuapp.com';
 export default host;
+
+let key = 0;
+export const securityQuestions = [
+  {
+    key: key++,
+    label: 'Mothers maiden name?',
+  },
+  {
+    key: key++,
+    label: 'Pet name?',
+  },
+  {
+    key: key++,
+    label: 'First childs birthyear?',
+  },
+];
