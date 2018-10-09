ea899d9be1979a38cf35bf1c620c0416488c62d5
AJ Genung <DragonRydrz@me.com>
2018-10-09 10:57:30 -0400

    added icons to bottomTabBar

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -1,16 +1,47 @@
+import React from 'react';
 import { createBottomTabNavigator } from 'react-navigation';
 import Options from '../Screens/Options';
 import NoteStack from './NoteStack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default createBottomTabNavigator({
-  NoteStack: {
-    screen: NoteStack,
-    navigationOptions: { title: 'Notes' },
-  },
-  Options: {
-    screen: Options,
-    navigationOptions: {
-      title: 'Settings',
+export default createBottomTabNavigator(
+  {
+    NoteStack: {
+      screen: NoteStack,
+      // navigationOptions: { title: 'Notes' },
+    },
+    Options: {
+      screen: Options,
+      // navigationOptions: {
+      //   title: 'Settings',
+      // },
     },
   },
-});
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'NoteStack') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Options') {
+          iconName = `ios-options${focused ? '' : '-outline'}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return (
+          <Ionicons
+            name={iconName}
+            size={horizontal ? 20 : 25}
+            color={tintColor}
+          />
+        );
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
